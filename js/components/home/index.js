175b29d3147351f26a56adc72ae86752490165d8
Allan Rabanillo <Allan Rabanillo>
2016-12-10 17:34:12 +0800

    First commit to Usave

---
+++ js/components/home/index.js
@@ -0,0 +1,157 @@
+
+import React, { Component } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { actions } from 'react-native-navigation-redux-helpers';
+import { Container, Header, Title, Content, Button, Icon, List, ListItem, InputGroup, Input, Picker, Text, Thumbnail ,Footer,FooterTab } from 'native-base';
+
+
+import { Grid, Row } from 'react-native-easy-grid';
+
+import { openDrawer } from '../../actions/drawer';
+import { setIndex } from '../../actions/list';
+import myTheme from '../../themes/base-theme';
+import styles from './styles';
+
+const Profile = require('../../../images/Profile.png');
+
+const QRcode = require('../../../images/Qrcode.png');
+
+const {
+  reset,
+  pushRoute,
+} = actions;
+
+class Home extends Component {
+
+  
+  static propTypes = {
+    name: React.PropTypes.string,
+    list: React.PropTypes.arrayOf(React.PropTypes.string),
+    setIndex: React.PropTypes.func,
+    openDrawer: React.PropTypes.func,
+    pushRoute: React.PropTypes.func,
+    reset: React.PropTypes.func,
+    navigation: React.PropTypes.shape({
+      key: React.PropTypes.string,
+    }),
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab1: false,
+      tab2: false,
+      tab3: true,
+      tab4: false,
+    };
+  }
+
+  toggleTab1() {
+    this.setState({
+      tab1: true,
+      tab2: false,
+      tab3: false,
+      tab4: false,
+    });
+  }
+
+  toggleTab2() {
+    this.setState({
+      tab1: false,
+      tab2: true,
+      tab3: false,
+      tab4: false,
+    });
+  }
+
+  toggleTab3() {
+    this.setState({
+      tab1: false,
+      tab2: false,
+      tab3: true,
+      tab4: false,
+    });
+  }
+
+  toggleTab4() {
+    this.setState({
+      tab1: false,
+      tab2: false,
+      tab3: false,
+      tab4: true,
+    });
+  }
+
+
+  pushRoute(route, index) {
+    this.props.setIndex(index);
+    this.props.pushRoute({ key: route, index: 1 }, this.props.navigation.key);
+  }
+
+  render() {
+    return (
+ 
+
+      <Container theme={myTheme} style={styles.container}>
+ <Header>
+          <Button transparent onPress={() => this.props.reset(this.props.navigation.key)}>
+            <Icon name="ios-power" />
+          </Button>
+
+          <Title>{(this.props.name) ? this.props.name : 'Home'}</Title>
+
+          <Button transparent onPress={this.props.openDrawer}>
+            <Icon name="ios-menu" />
+          </Button>
+        </Header>
+
+        <Content>
+         
+
+          
+          </Content>
+
+        <Footer >
+          <FooterTab>
+          <Button active={this.state.tab4} onPress={() => this.toggleTab4()} >
+                Home
+              <Icon name="ios-home-outline" />
+            </Button>
+            <Button active={this.state.tab1} onPress={() => this.toggleTab1()} >
+                Plans
+              <Icon name="ios-clipboard-outline" />
+            </Button>
+            <Button active={this.state.tab2} onPress={() => this.toggleTab2()} >
+                Profile
+              <Icon name="ios-person-outline" />
+            </Button>
+            <Button active={this.state.tab3} onPress={() => this.toggleTab3()} >
+                Settings
+              <Icon name="ios-paper-outline" />
+            </Button>
+            
+          </FooterTab>
+        </Footer>
+      </Container>
+
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+    setIndex: index => dispatch(setIndex(index)),
+    openDrawer: () => dispatch(openDrawer()),
+    pushRoute: (route, key) => dispatch(pushRoute(route, key)),
+    reset: key => dispatch(reset([{ key: 'login' }], key, 0)),
+  };
+}
+
+const mapStateToProps = state => ({
+  name: state.user.name,
+  list: state.list.list,
+  navigation: state.cardNavigation,
+});
+
+export default connect(mapStateToProps, bindAction)(Home);
